0b796882192bc6434a5e5295a5b601b0662386ce
Anthony Scopatz <scopatz@gmail.com>
2019-03-15 12:59:43 -0400

    generate from pygments

---
+++ from-pygments.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Generates languages, styles, etc. from pygments for use with
+source-highlight.
+"""
+
+def main(args=None):
+    pass
+
+
+if __name__ == "__main__":
+    main()
